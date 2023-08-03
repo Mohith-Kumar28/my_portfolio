71f3e20eb7978a999bf253e94ae2248267e0288a
Mohith kumar <murugan28aug@gmail.com>
2023-08-04 00:51:51 +0530

    test final

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   
 
   <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
-<Script strategy="lazyOnload">
+<Script id="analytics-script" strategy="lazyOnload">
 {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
