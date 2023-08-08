b370beb335d0bd66eca0c0b50c2e252cf86393e0
Mohith kumar <murugan28aug@gmail.com>
2023-08-09 02:09:08 +0530

    about text

--- components/about/Intro.jsx
+++ components/about/Intro.jsx
@@ -12,7 +12,7 @@ const Intro = () => {
     <div className='md:w-2/3'>
     <p className='text-pri-gray py-8 md:pr-16'>
     Hello, I amm Mohith!
-     A passionate and self-taught front-end developer located in Hyderabad, India. With a strong flair for creativity and web development, I excel at crafting responsive and user-friendly websites from scratch.<br/><br/> Over the past year, I have been dedicated to transforming my knowledge into captivating web experiences, assisting numerous clients in establishing their online presence. Always eager to stay up-to-date with the latest technologies and frameworks, I am committed to delivering innovative solutions for the digital world.
+     A passionate and self-taught MERN stack developer located in Hyderabad, India. With a strong flair for creativity and web development, I excel at crafting responsive and user-friendly websites from scratch.<br/><br/> Over the past year, I have been dedicated to transforming my knowledge into captivating web experiences, assisting numerous clients in establishing their online presence. Always eager to stay up-to-date with the latest technologies and frameworks, I am committed to delivering innovative solutions for the digital world.
     </p></div>
 <div className=' w-full md:w-1/3'>
     <Image blurDataURL={'/assets/about/aboutMePic.png'} placeholder='blur' alt='profilepic' width={700} height={700} src={'/assets/about/aboutMePic.png'}/>
